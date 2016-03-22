31493fb29f45f19f506ef29f73fee34c72aa9af6
Wenbo Li <wenbolovesnz@gmail.com>
2016-03-22 23:28:09 +1300

    implemented mergeSort simple version

--- mergeSort.js
+++ mergeSort.js
@@ -1,39 +1,46 @@
 //simple merge sort without error handling.
+var unsortedArray = [2, 4, 6, 1, 5, 9];
 
-var unsortedArray = [2,4,6,1,3,7,1,6,5];
+var merge = function(array1, array2){
 
-var swap = function(array, index){
-	var a = array[index];
-	var b = array[index - 1];
-	var temp = a;
-	array[index] = b;
-	array[index-1] = temp;		
+    mergedArray = [];
+    while(array1.length !== 0 && array2.length !==0){
+
+        if(array1[0] > array2[0]){
+            var smallerFrom2 = array2.shift();
+            mergedArray.push(smallerFrom2);
+
+        }else if(array1[0] < array2[0]){
+            var smallerFrom1 = array1.shift();
+            mergedArray.push(smallerFrom1);
+        }else{
+
+        }
+    }
+
+    if(array1.length === 0){
+        mergedArray = mergedArray.concat(array2)
+    }else{
+        mergedArray = mergedArray.concat(array1)
+    }
+
+    return mergedArray;
+};
+
+function sort(array) {
+
+    var length = array.length,
+        mid    = Math.floor(length * 0.5),
+        left   = array.slice(0, mid),
+        right  = array.slice(mid, length);
+
+    if(length === 1) {
+        return array;
+    }
+
+    return merge(sort(left), sort(right));
+
 }
 
-var recursiveSwap = function(array, index){
-	if(index === 0){
-		return true; //done
-	}
-	
-	if(array[index] >= array[index-1]){
-		return true; // done as well
-	}
-	
-	swap(array, index);	
-	recursiveSwap(array, index -1);	
-}
-
-var insertionSort = function(array){
-	
-	for(var i = 1; i < array.length; i++){	
-		if(array[i] < array[i-1]){
-			recursiveSwap(array, i);																
-		}		
-	}		
-	
-	return array;
-}
-
-var sortedArray = insertionSort(unsortedArray);
-
-console.log(sortedArray);
+var a = sort(unsortedArray);
+console.log(a);
